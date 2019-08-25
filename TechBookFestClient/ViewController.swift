9b4acd7b2922e4003d4721e1e6b6aec0df403645
ShoMasegi <m2.b0c27@gmail.com>
2019-08-26 00:04:41 +0900

    Initial Commit

---
+++ TechBookFestClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechBookFestClient
+//
+//  Created by ShoMasegi on 2019/08/26.
+//  Copyright © 2019 ShoMasegi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
