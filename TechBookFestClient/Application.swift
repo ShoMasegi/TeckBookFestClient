af6032733cdca625026f9d02678546f462e7c744
ShoMasegi <m2.b0c27@gmail.com>
2019-08-29 21:45:50 +0900

    Implement Application.newDefaultUseCaseProvider

--- TechBookFestClient/Application.swift
+++ TechBookFestClient/Application.swift
@@ -14,7 +14,23 @@ final class Application {
         window.makeKeyAndVisible()
     }
 
-    func defaultUseCaseProvider() {
-
+    func defaultUseCaseProvider() -> UseCaseProvider {
+        let networking = Networking.newDefaultNetworking(responseFilterClosure: { statusCode -> Bool in
+            switch statusCode {
+            case 426:
+                let lockViewController = LockViewController.instantiateFromStoryboard()
+                lockViewController.type = .versionUpdate
+                self.rootViewController.animateFadeTransition(to: lockViewController)
+                return false
+            case 503:
+                let lockViewController = LockViewController.instantiateFromStoryboard()
+                lockViewController.type = .maintenance
+                self.rootViewController.animateFadeTransition(to: lockViewController)
+                return false
+            default:
+                return true
+            }
+        })
+        return UseCaseProvider(networking: networking)
     }
 }
