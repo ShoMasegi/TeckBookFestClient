44f68c8d52328edfc7c5e562d53b5d2aca023b9e
ShoMasegi <m2.b0c27@gmail.com>
2019-08-29 21:41:57 +0900

    Apply RootViewController to FadeTransitionable

--- TechBookFestClient/Scenes/RootViewController.swift
+++ TechBookFestClient/Scenes/RootViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class RootViewController: UIViewController {
+final class RootViewController: UIViewController, FadeTransitionable {
     var current: UIViewController?
 
     init() {
@@ -19,38 +19,5 @@ final class RootViewController: UIViewController {
 
     override var childForStatusBarHidden: UIViewController? {
         return current
-    }
-
-    func setupCurrent() {
-        guard let current = self.current else {
-            return
-        }
-        addChild(current)
-        current.view.frame = view.bounds
-        view.addSubview(current.view)
-        current.didMove(toParent: self)
-    }
-
-    func animateFadeTransition(to toViewController: UIViewController, completion: (() -> Void)? = nil) {
-        guard let current = self.current else {
-            return
-        }
-        current.willMove(toParent: nil)
-        addChild(toViewController)
-        transition(
-            from: current,
-            to: toViewController,
-            duration: 0.3,
-            options: [.transitionCrossDissolve, .curveEaseOut],
-            animations: nil,
-            completion: { [weak self] _ in
-                guard let self = self else { return }
-                self.current?.removeFromParent()
-                toViewController.didMove(toParent: self)
-                self.current = toViewController
-                self.setNeedsStatusBarAppearanceUpdate()
-                completion?()
-            }
-        )
     }
 }
