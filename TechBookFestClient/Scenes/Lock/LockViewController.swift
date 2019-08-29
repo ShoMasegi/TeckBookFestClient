24dbd6ffd6c167e35908b2e388df60931ba8b72d
ShoMasegi <m2.b0c27@gmail.com>
2019-08-29 21:43:20 +0900

    Add LockViewController

---
+++ TechBookFestClient/Scenes/Lock/LockViewController.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+final class LockViewController: UIViewController, StoryboardInstantiable {
+    enum LockType {
+        case maintenance
+        case versionUpdate
+        var title: String {
+            switch self {
+            case .maintenance: return "メンテナンスのお知らせ"
+            case .versionUpdate: return "アプリアップデートのお知らせ"
+            }
+        }
+
+        var body: String {
+            switch self {
+            case .maintenance:
+                return "現在メンテナンス中です。復旧までしばらくお待ち下さい。"
+            case .versionUpdate:
+                return "アプリを安定にご利用いただくため、最新バージョンへのアップデートをお願いします。"
+            }
+        }
+
+        var buttonTitle: String {
+            switch self {
+            case .maintenance: return "お知らせを確認"
+            case .versionUpdate: return "更新する"
+            }
+        }
+
+        var url: URL? {
+            return URL(string: "https://github.com/ShoMasegi/TechBookFestClient")
+        }
+    }
+
+    var type: LockType?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = type?.title
+        bodyLabel.text = type?.body
+        button.setTitle(type?.buttonTitle, for: .normal)
+    }
+
+    @IBOutlet private weak var alertView: UIView! {
+        didSet {
+            alertView.layer.cornerRadius = 10
+            alertView.clipsToBounds = true
+            alertView.layer.shadowColor = UIColor(white: 0, alpha: 0.5).cgColor
+            alertView.layer.shadowOffset = CGSize(width: 0, height: 4.0)
+            alertView.layer.shadowRadius = 10
+            alertView.layer.shouldRasterize = true
+            alertView.layer.rasterizationScale = UIScreen.main.scale
+            alertView.layer.shadowOpacity = 1
+            alertView.layer.masksToBounds = false
+        }
+    }
+    @IBOutlet private weak var bodyLabel: UILabel!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var button: UIButton! {
+        didSet {
+            button.setTitleColor(.gray, for: .highlighted)
+        }
+    }
+
+    @IBAction private func buttonTouchUpInside(_ sender: UIButton) {
+        guard let url = type?.url else { return }
+        UIApplication.shared.open(url)
+    }
+}
