5a25efdcb5131642e0e670126a130e35eecd09e4
ShoMasegi <m2.b0c27@gmail.com>
2019-08-30 16:02:21 +0900

    Add HomePresenter

--- TechBookFestClient/Scenes/Home/HomeViewController.swift
+++ TechBookFestClient/Scenes/Home/HomeViewController.swift
@@ -3,6 +3,22 @@ import UIKit
 final class HomeViewController: UIViewController, StoryboardInstantiable {
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter.fetch()
     }
+
+    private lazy var presenter: HomePresenterInput = HomePresenter(
+        output: self,
+        useCase: UseCaseProvider(networking: Networking.newStubNetworking()).makeHomeUseCase()
+    )
 }
 
+extension HomeViewController: HomePresenterOutput {
+    func fetched(result: Result<Home, Error>) {
+        switch result {
+        case .success(let home):
+            print(home)
+        case .failure(let error):
+            print(error)
+        }
+    }
+}
