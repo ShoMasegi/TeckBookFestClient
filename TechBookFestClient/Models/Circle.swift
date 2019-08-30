ef63358f0d2f096a83a549800760a73345de66e1
ShoMasegi <m2.b0c27@gmail.com>
2019-08-30 16:39:55 +0900

    Fix Circle model

--- TechBookFestClient/Models/Circle.swift
+++ TechBookFestClient/Models/Circle.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 struct Circle: Decodable {
+    let id: Int
     let title: String
     let circleType: CircleType
+    let position: String
     let category: String
+    let description: String
     let imageUrl: String
 }
 
