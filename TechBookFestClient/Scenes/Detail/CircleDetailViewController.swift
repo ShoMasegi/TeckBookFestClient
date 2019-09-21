a33905054d831f0e070a5769954e390c67fa149c
ShoMasegi <m2.b0c27@gmail.com>
2019-09-22 07:56:48 +0900

    Add CircleDetail Scene

---
+++ TechBookFestClient/Scenes/Detail/CircleDetailViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class CircleDetailViewController: UIViewController {
+    @IBOutlet private var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
